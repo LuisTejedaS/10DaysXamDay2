b97af4401ac31bf010e530a691e9a46008e9dbc4
Luis Tejeda <luis.tejeda@contpaqi.com>
2019-03-19 10:18:38 -0600

    styles

---
+++ src/TenXamarinD2/TenXamarinD2/TenXamarinD2/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TenXamarinD2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckIfShouldBeEnabled()
+        {
+            saveButton.IsEnabled = false;
+            if (!string.IsNullOrWhiteSpace(titleEntry.Text) && !string.IsNullOrWhiteSpace(contentEditor.Text))
+                saveButton.IsEnabled = true;
+        }
+
+        private void TitleEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckIfShouldBeEnabled();
+        }
+
+        private void ContentEditor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckIfShouldBeEnabled();
+        }
+
+        private void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            titleEntry.Text = string.Empty;
+            contentEditor.Text = string.Empty;
+        }
+    }
+}
